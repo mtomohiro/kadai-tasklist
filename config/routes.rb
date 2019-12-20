3c45d02c3db0966c449f8c10dcad7cbc14a6812f
nan <triplesouth.dev@gmail.com>
2019-12-20 02:59:51 +0000

    create session , login/logout , signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/new'
   get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,4 +10,9 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:new, :create]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
 end
