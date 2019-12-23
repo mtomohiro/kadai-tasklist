e16dc2895437f8aa355149b1933c52ead1ef7cf4
nan <triplesouth.dev@gmail.com>
2019-12-23 06:43:38 +0000

    bugfix Unauthorized access , delete unnecessary routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
+  #get 'sessions/new'
+  #get 'sessions/create'
+  #get 'sessions/destroy'
+  #get 'users/new'
+  #get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   root to: 'tasks#index'
   resources :tasks
   
